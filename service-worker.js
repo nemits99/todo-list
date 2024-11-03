42f50e20883a1f7d93c4f4029fea164900b8f40b
nemits99 <nemit.sadamawi@gmail.com>
2024-11-04 06:55:14 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'todo-app-cache';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './style.css', // add any additional CSS files
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
